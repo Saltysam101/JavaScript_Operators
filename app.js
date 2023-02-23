faeac9e8b247a25184935b9af76c618d54ab0d15
Sam Zudell <s.zudell98@gmail.com>
2023-02-23 08:24:34 -0500

    did exercise

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
 console.log("Hello World!\n==========\n");
 console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+    "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20
+let b = 4
 
-// Exercise 2
+let add = a + b
+let minus = a - b
+let multiply = a * b
+let divide = a / b
+
+console.log(add, minus, multiply, divide)
+    // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
-
-// Exercise 3
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+console.log(num + str, num + str2, num + isPresent, isPresent + str, firstName + lastName)
+    /*
+    1111
+    11eleven
+    12
+    true11
+    FrodoBaggins
+    */
+    // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+/*
+true
+false
+true
+false
+true
+true
+false
+false
+true
+*/
